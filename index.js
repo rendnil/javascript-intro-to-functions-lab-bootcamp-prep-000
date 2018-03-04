e2d12cc635c2c6ea1c44aeadbf8c819ceeb4910f
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-03-04 19:28:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   
-  if string.toLowerCase() === string {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
   
